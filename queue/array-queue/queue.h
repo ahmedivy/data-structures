2818d3595822841b0409a14325b21a011baded12
ahmed <ahmadabdullah4919@gmail.com>
2023-03-05 22:22:59 +0500

    Queue Impl.

---
+++ queue/array-queue/queue.h
@@ -0,0 +1,77 @@
+#pragma once
+
+#include <iostream>
+
+using namespace std;
+
+template<class T>
+class Queue
+{
+    private:
+        int front;
+        int back;
+        int size;
+        T *array;
+
+    public:
+        Queue(int size)
+        {
+            this->size = size;
+            this->front = -1;
+            this->back = -1;
+            this->array = new T[size];
+        }
+
+        ~Queue()
+        {
+            delete[] array;
+        }
+
+        bool isEmpty()
+        {
+            return front == -1 && back == -1;
+        }
+
+        bool isFull()
+        {
+            return back + 1 == size;
+        }
+
+        void push(T value)
+        {
+            if (isFull())
+            {
+                throw runtime_error("Queue is Full!");
+            }
+            if (isEmpty())
+            {
+                this->front++;
+            }
+            array[++this->back] == value;
+        }
+
+        T pop()
+        {
+            if (isEmpty())
+            {
+                throw runtime_error("Queue is Empty!");
+            }
+            if (this->front == this->back)
+            {
+                T value = array[this->front];
+                this->front = -1;
+                this->back = -1;
+                return value;
+            }
+            return array[this->front--];
+        }
+
+        T peek()
+        {
+            if (isEmpty())
+            {
+                throw runtime_error("Queue is Empty!");
+            }
+            return array[this->front];
+        }
+};
