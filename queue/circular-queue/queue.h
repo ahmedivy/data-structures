def81c0be4af80b1382aae33a9b6f0c36adee660
ahmed <ahmadabdullah4919@gmail.com>
2023-03-06 14:42:15 +0500

    Circular Queue Impl.

--- queue/circular-queue/queue.h
+++ queue/circular-queue/queue.h
@@ -12,4 +12,101 @@ private:
     int front;
     int rear;
 
-pu
+public:
+    Queue(int size)
+    {
+        this->size = size;
+        arr = new T[size];
+        front = -1;
+        rear = -1;
+    }
+
+    ~Queue()
+    {
+        delete[] arr;
+    }
+
+    bool isEmpty()
+    {
+        return front == -1 && rear == -1;
+    }
+
+    bool isFull()
+    {
+        return (rear + 1) % size == front;
+    }
+
+    void enqueue(T data)
+    {
+        if (isFull())
+        {
+            cout << "Queue is full" << endl;
+            return;
+        }
+        else if (isEmpty())
+        {
+            front = rear = 0;
+        }
+        else
+        {
+            rear = (rear + 1) % size;
+        }
+        arr[rear] = data;
+    }
+
+    T dequeue()
+    {
+        T data = 0;
+        if (isEmpty())
+        {
+            cout << "Queue is empty" << endl;
+            return data;
+        }
+        else if (front == rear)
+        {
+            data = arr[front];
+            front = rear = -1;
+        }
+        else
+        {
+            data = arr[front];
+            front = (front + 1) % size;
+        }
+        return data;
+    }
+
+    T peek()
+    {
+        if (isEmpty())
+        {
+            cout << "Queue is empty" << endl;
+            return 0;
+        }
+        return arr[front];
+    }
+
+    int count()
+    {
+        return (size - front + rear + 1) % size;
+    }
+
+    string toString()
+    {
+        string result = "";
+        if (isEmpty())
+        {
+            return "Queue is empty";
+        }
+        else
+        {
+            int i = front;
+            while (i != rear)
+            {
+                result += to_string(arr[i]) + " ";
+                i = (i + 1) % size;
+            }
+            result += to_string(arr[i]);
+        }
+        return result;
+    }
+};
