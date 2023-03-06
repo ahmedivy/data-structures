92c994e6a3a34cf37c860ebe037df4120b514e3a
ahmed <ahmadabdullah4919@gmail.com>
2023-03-06 15:36:50 +0500

    Add Menu to Test

--- stack/array-stack/main.cpp
+++ stack/array-stack/main.cpp
@@ -3,20 +3,45 @@
 
 using namespace std;
 
+void printMenu();
+
 int main()
 {
-    Stack<int> stack(10);
-    cout << stack;
-    stack.push(10);
-    stack.push(30);
-    stack.push(35);
-    stack.push(43);
-    cout << stack;
-    stack.pop();
-    cout << stack;
-    stack.push(123);
-    stack.push(245);
-    cout << stack;
+    printMenu();
 
     return 0;
+}
+
+void printMenu()
+{
+    Stack<int> s(30);
+    int choice;
+    int data;
+    do
+    {
+        cout << "1. Push" << endl;
+        cout << "2. Pop" << endl;
+        cout << "3. Display" << endl;
+        cout << "4. Exit" << endl;
+        cout << "Enter your choice: ";
+        cin >> choice;
+        switch (choice)
+        {
+        case 1:
+            cout << "Enter the data: ";
+            cin >> data;
+            s.push(data);
+            break;
+        case 2:
+            cout << "Popped : " << s.pop() << endl;
+            break;
+        case 3:
+            cout << "Stack:" << s.display() << endl;
+            break;
+        case 4:
+            exit(0);
+        default:
+            cout << "Invalid choice" << endl;
+        }
+    } while (choice != 4);
 }
