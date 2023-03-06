b397c2606d1774cfda55aae45afed299ca9404d6
ahmed <ahmadabdullah4919@gmail.com>
2023-03-06 15:09:55 +0500

    Test Circular Queue

---
+++ queue/circular-queue/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include "queue.h"
+
+using namespace std;
+
+void printMenu();
+
+int main()
+{
+    printMenu();
+
+    return 0;
+}
+
+void printMenu()
+{
+    Queue<int> q(30);
+    int choice;
+    int data;
+    do
+    {
+        cout << "1. Enqueue" << endl;
+        cout << "2. Dequeue" << endl;
+        cout << "3. Display" << endl;
+        cout << "4. Exit" << endl;
+        cout << "Enter your choice: ";
+        cin >> choice;
+        switch (choice)
+        {
+        case 1:
+            cout << "Enter the data: ";
+            cin >> data;
+            q.enqueue(data);
+            break;
+        case 2:
+            cout << q.dequeue() << endl;
+            break;
+        case 3:
+            cout << "Queue:" << q.display() << endl;
+            break;
+        case 4:
+            exit(0);
+        default:
+            cout << "Invalid choice" << endl;
+        }
+    } while (choice != 4);
+}
