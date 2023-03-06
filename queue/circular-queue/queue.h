ae2c99819fec3e56e69360a2f2e3071a5fc6747b
ahmed <ahmadabdullah4919@gmail.com>
2023-03-06 15:09:27 +0500

    Bug Fix

--- queue/circular-queue/queue.h
+++ queue/circular-queue/queue.h
@@ -6,107 +6,111 @@ using namespace std;
 template <class T>
 class Queue
 {
-private:
-    T *arr;
-    int size;
-    int front;
-    int rear;
+    private:
+        T *arr;
+        int size;
+        int front;
+        int rear;
 
-public:
-    Queue(int size)
-    {
-        this->size = size;
-        arr = new T[size];
-        front = -1;
-        rear = -1;
-    }
-
-    ~Queue()
-    {
-        delete[] arr;
-    }
-
-    bool isEmpty()
-    {
-        return front == -1 && rear == -1;
-    }
-
-    bool isFull()
-    {
-        return (rear + 1) % size == front;
-    }
-
-    void enqueue(T data)
-    {
-        if (isFull())
+    public:
+        Queue(int size)
         {
-            cout << "Queue is full" << endl;
-            return;
+            this->size = size;
+            arr = new T[size];
+            front = -1;
+            rear = -1;
         }
-        else if (isEmpty())
-        {
-            front = rear = 0;
-        }
-        else
-        {
-            rear = (rear + 1) % size;
-        }
-        arr[rear] = data;
-    }
 
-    T dequeue()
-    {
-        T data = 0;
-        if (isEmpty())
+        ~Queue()
         {
-            cout << "Queue is empty" << endl;
+            delete[] arr;
+        }
+
+        bool isEmpty()
+        {
+            return front == -1 && rear == -1;
+        }
+
+        bool isFull()
+        {
+            if(isEmpty())
+            {
+                return false;
+            }
+            return (front == 0 && rear == size - 1) || (front == rear + 1);
+        }
+
+        void enqueue(T data)
+        {
+            if (isFull())
+            {
+                cout << "Queue is full" << endl;
+                return;
+            }
+            else if (isEmpty())
+            {
+                front = rear = 0;
+            }
+            else
+            {
+                rear = (rear + 1) % size;
+            }
+            arr[rear] = data;
+        }
+
+        T dequeue()
+        {
+            T data = 0;
+            if (isEmpty())
+            {
+                cout << "Queue is empty" << endl;
+                return data;
+            }
+            else if (front == rear)
+            {
+                data = arr[front];
+                front = rear = -1;
+            }
+            else
+            {
+                data = arr[front];
+                front = (front + 1) % size;
+            }
             return data;
         }
-        else if (front == rear)
-        {
-            data = arr[front];
-            front = rear = -1;
-        }
-        else
-        {
-            data = arr[front];
-            front = (front + 1) % size;
-        }
-        return data;
-    }
 
-    T peek()
-    {
-        if (isEmpty())
+        T peek()
         {
-            cout << "Queue is empty" << endl;
-            return 0;
-        }
-        return arr[front];
-    }
-
-    int count()
-    {
-        return (size - front + rear + 1) % size;
-    }
-
-    string toString()
-    {
-        string result = "";
-        if (isEmpty())
-        {
-            return "Queue is empty";
-        }
-        else
-        {
-            int i = front;
-            while (i != rear)
+            if (isEmpty())
             {
-                result += to_string(arr[i]) + " ";
-                i = (i + 1) % size;
+                cout << "Queue is empty" << endl;
+                return 0;
             }
-            result += to_string(arr[i]);
+            return arr[front];
         }
-        return result;
-    }
+
+        int count()
+        {
+            return (size - front + rear + 1) % size;
+        }
+
+        string display()
+        {
+            string result = "";
+            if (isEmpty())
+            {
+                return "Queue is empty";
+            }
+            else
+            {
+                int i = front;
+                while (i != rear)
+                {
+                    result += to_string(arr[i]) + " ";
+                    i = (i + 1) % size;
+                }
+                result += to_string(arr[i]);
+            }
+            return result;
+        }
 };
