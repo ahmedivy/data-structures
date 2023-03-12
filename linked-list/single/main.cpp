1a69856da534cd6b0898a736371b97beae6a9e93
ahmed <ahmadabdullah4919@gmail.com>
2023-03-12 14:43:49 +0500

    Test methods

--- linked-list/single/main.cpp
+++ linked-list/single/main.cpp
@@ -6,7 +6,19 @@ int main()
 {
     LinkedList<int> list;
     std::cout << "Linked List: " << list.display() << std::endl;
-    list.insert(42);
+    list.insertBack(42);
+    std::cout << "Linked List: " << list.display() << std::endl;
+    list.insertBack(60);
+    std::cout << "Linked List: " << list.display() << std::endl;
+    list.insertFront(34);
+    std::cout << "Linked List: " << list.display() << std::endl;
+    list.popFront();
+    list.popFront();
+    list.popFront();
+    std::cout << "Linked List: " << list.display() << std::endl;
+    list.insertBack(12);
+    list.insertBack(17);
+    list.insertBack(23);
     std::cout << "Linked List: " << list.display() << std::endl;
     return 0;
 }
