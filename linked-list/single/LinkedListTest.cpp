f4cb68b1949bf07d63192c23c75d81c8b91b3d67
ahmed <ahmadabdullah4919@gmail.com>
2023-03-13 15:50:21 +0500

    Add Tests

---
+++ linked-list/single/LinkedListTest.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <cassert>
+#include "LinkedList.h"
+
+void runTests();
+
+int main()
+{
+    try
+    {
+        runTests();
+        std::cout << "All tests passed!" << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+}
+
+void runTests()
+{
+    LinkedList<int> ll;
+    assert(ll.isEmpty());
+    ll.insertFront(56);
+    assert(!ll.isEmpty());
+    assert(ll.length() == 1);
+    assert(ll.search(56) == 0);
+    assert(ll.search(7) == -1);
+    ll.insertBack(7);
+    assert(ll.length() == 2);
+    assert(ll.search(7) == 1);
+}
