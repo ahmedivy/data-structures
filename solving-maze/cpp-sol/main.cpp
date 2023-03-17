c1b24f0375a5c5094cf600d4288fdb5e69914972
ahmed <ahmadabdullah4919@gmail.com>
2023-03-17 22:33:54 +0500

    Add Main Class

--- solving-maze/cpp-sol/main.cpp
+++ solving-maze/cpp-sol/main.cpp
@@ -1,26 +1,31 @@
 #include <iostream>
 #include <string>
 
-#include "SolveMaze.h"
+#include "solvemaze.h"
 
-int main(int argc, char* argv[]) {
-    if (argc < 2) {
-        std::cerr << "Usage: " << argv[0] << " <maze file>" << std::endl;
-        return 1;
-    }
+int main(int argc, char* argv[]) 
+{
+    // if (argc < 2) 
+    // {
+    //     std::cerr << "Usage: " << argv[0] << " <maze file>" << std::endl;
+    //     return 1;
+    // }
 
-    SolveMaze solver(argv[1]);
+    SolveMaze* solver = new SolveMaze("puzzles/maze2.txt");
 
     // Print the maze before solving it
     std::cout << "Maze before solving: " << std::endl << std::endl;
-    solver.printMaze();
+    solver->printMaze();
 
     // Use the solver object to solve the maze
-    solver.solve();
 
+    solver->solve();
+    
     // Print the maze after solving it
     std::cout << "Maze after solving: " << std::endl << std::endl;
-    solver.printMaze();
+    solver->printMaze();
+
+    delete solver;
 
     return 0;
 }
