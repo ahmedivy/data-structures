fade02e3b5814a3de08f8af45b0bb3b6283094ce
ahmed <ahmadabdullah4919@gmail.com>
2023-03-17 22:35:19 +0500

    Add arg support

--- solving-maze/cpp-sol/main.cpp
+++ solving-maze/cpp-sol/main.cpp
@@ -5,27 +5,27 @@
 
 int main(int argc, char* argv[]) 
 {
-    // if (argc < 2) 
-    // {
-    //     std::cerr << "Usage: " << argv[0] << " <maze file>" << std::endl;
-    //     return 1;
-    // }
+    if (argc < 2) 
+    {
+        std::cerr << "Usage: " << argv[0] << " <maze file>" << std::endl;
+        return 1;
+    }
 
-    SolveMaze* solver = new SolveMaze("puzzles/maze2.txt");
+    SolveMaze* solver = new SolveMaze(argv[1]);
 
     // Print the maze before solving it
     std::cout << "Maze before solving: " << std::endl << std::endl;
     solver->printMaze();
+    std::cout << std::endl;
 
     // Use the solver object to solve the maze
-
     solver->solve();
     
     // Print the maze after solving it
     std::cout << "Maze after solving: " << std::endl << std::endl;
     solver->printMaze();
+    std::cout << std::endl;
 
     delete solver;
-
     return 0;
 }
