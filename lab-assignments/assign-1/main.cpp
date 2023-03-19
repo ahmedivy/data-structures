acf832fe413ab3cc9a52fe331965cad1d3d97c26
ahmed <ahmadabdullah4919@gmail.com>
2023-03-19 20:56:41 +0500

    Test rotate method

--- lab-assignments/assign-1/main.cpp
+++ lab-assignments/assign-1/main.cpp
@@ -7,18 +7,41 @@ template <typename T>
 using Node = typename LinkedList<T>::Node;
 
 template <typename T>
-Node<T>* copy(Node* list);
+Node<T>* copyList(Node<T>* list);
 
 int main()
 {
-    LinkedList<int> list;
-    
+    LinkedList<int>* list = new LinkedList<int>();
+    list->insertBack(2);
+    list->insertBack(4);
 
+    LinkedList<int>* copyList = list->copy();
+
+    LinkedList<int>* listTwo = new LinkedList<int>();
+    listTwo->insertBack(6);
+    listTwo->insertBack(8);
+
+    LinkedList<int>* concatented = list->concat(listTwo);
+    concatented->display();
+
+    delete copyList;
+    delete list;
+    delete listTwo;
+
+    concatented->display();
+    concatented->replace(2, 5);
+    concatented->display();
+    std::cout << concatented->secondToLast() << std::endl;
+    concatented->rotate(2);
+    concatented->display();
+
+
+    delete concatented;
     return 0;
 }
 
 template <typename T>
-Node<T>* copy(const Node<T>* list)
+Node<T>* copyList(const Node<T>* list)
 {
     if (list == nullptr)
     {
@@ -31,7 +54,7 @@ Node<T>* copy(const Node<T>* list)
 
     while (current != nullptr)
     {
-        Node<T>* newNode = new Node(current->data);
+        Node<T>* newNode = new Node<T>(current->data);
         newNode->next = newTail;
         newTail = newNode;
         current = current->next;
