d1ac9ed653089c354d9e7f94d1e1d81787126171
ahmed <ahmadabdullah4919@gmail.com>
2023-03-19 18:12:57 +0500

    Add copy method

--- lab-assignments/assign-1/main.cpp
+++ lab-assignments/assign-1/main.cpp
@@ -3,9 +3,44 @@
 
 #include "LinkedList.h"
 
+template <typename T>
+using Node = typename LinkedList<T>::Node;
+
+template <typename T>
+Node<T>* copy(Node* list);
+
 int main()
 {
-    std::cout << "Hello World!" << std::endl;
+    LinkedList<int> list;
+    
 
     return 0;
+}
+
+template <typename T>
+Node<T>* copy(const Node<T>* list)
+{
+    if (list == nullptr)
+    {
+        return nullptr;
+    }
+
+    Node<T>* current = list;
+    Node<T>* newHead = nullptr;
+    Node<T>* newTail = nullptr;
+
+    while (current != nullptr)
+    {
+        Node<T>* newNode = new Node(current->data);
+        newNode->next = newTail;
+        newTail = newNode;
+        current = current->next;
+
+        if (newHead == nullptr)
+        {
+            newHead = newNode;
+        }
+    }
+
+    return newHead;
 }
