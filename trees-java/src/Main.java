cb6c9b67123d31834897b234ad7f281205f88aab
ahmed <ahmadabdullah4919@gmail.com>
2023-05-24 10:08:14 +0500

    Add remove method

--- trees-java/src/Main.java
+++ trees-java/src/Main.java
@@ -5,6 +5,10 @@ public class Main {
         AVLTree tree = new AVLTree(20);
         tree.add(30);
         tree.add(40);
+        tree.add(50);
+        tree.add(25);
+        System.out.println(tree);
+        tree.remove(30);
         System.out.println(tree);
     }
 }
--- trees-java/src/Trees/AVLTree.java
+++ trees-java/src/Trees/AVLTree.java
@@ -17,12 +17,14 @@ public class AVLTree {
     }
 
     public int size() {
-        if (this == NIL) return 0;
+        if (this == NIL)
+            return 0;
         return 1 + left.size() + right.size();
     }
 
     public String toString() {
-        if (this == NIL) return "";
+        if (this == NIL)
+            return "";
         return left + " " + key + " " + right;
     }
 
@@ -40,10 +42,14 @@ public class AVLTree {
     }
 
     public AVLTree grow(int key) {
-        if (this == NIL) return new AVLTree(key);
-        if (key == this.key) return this;
-        if (key < this.key) left = left.grow(key);
-        else right = right.grow(key);
+        if (this == NIL)
+            return new AVLTree(key);
+        if (key == this.key)
+            return this;
+        if (key < this.key)
+            left = left.grow(key);
+        else
+            right = right.grow(key);
         rebalance();
         height = 1 + Math.max(left.height, right.height);
         return this;
@@ -76,5 +82,48 @@ public class AVLTree {
         key = left.key;
         left = left.left;
         return this;
+    }
+
+    public boolean remove(int key) {
+        int oldSize = size();
+        shrink(key);
+        return size() < oldSize;
+    }
+
+    public AVLTree shrink(int key) {
+        if (this == NIL)
+            return NIL;
+        if (key == this.key) {
+            if (left == NIL && right == NIL)
+                return NIL;
+            if (left == NIL)
+                return right;
+            if (right == NIL)
+                return left;
+            this.key = right.min().key;
+            right = right.removeMin();
+        } else if (key < this.key) {
+            left = left.shrink(key);
+        } else {
+            right = right.shrink(key);
+        }
+        rebalance();
+        height = 1 + Math.max(left.height, right.height);
+        return this;
+    }
+
+    public AVLTree removeMin() {
+        if (left == NIL)
+            return right;
+        left = left.removeMin();
+        rebalance();
+        height = 1 + Math.max(left.height, right.height);
+        return this;
+    }
+
+    public AVLTree min() {
+        if (left == NIL)
+            return this;
+        return left.min();
     }
 }
